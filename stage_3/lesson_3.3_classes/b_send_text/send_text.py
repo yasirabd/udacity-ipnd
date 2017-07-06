8126d847edbe0cd9296f6854bb498fc512a44487
yasirabd <sir.yasirabd@gmail.com>
2017-07-06 23:40:22 +0700

    twilio (failed) maybe because using trial version number

--- stage_3/lesson_3.3_classes/b_send_text/send_text.py
+++ stage_3/lesson_3.3_classes/b_send_text/send_text.py
@@ -6,6 +6,20 @@
 # to send a text message using Twilio, a library we'll
 # download from the Internet and add to Python.
 
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 
 # Your code here.
+
+# Your Account SID from twilio.com/console
+account_sid = "ACeefc945ce545d8724afcf01b0bd88230"
+# Your Auth Token from twilio.com/console
+auth_token  = "4e9b3ffeabc1c8bda6e5686523cee1bf"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+6285729977530",
+    from_="+6285574677326",
+    body="Hello from Python!")
+
+print(message.sid)
