21e37fc1c47e0a889a0bdb016aa916b65c3eee3d
yasirabd <sir.yasirabd@gmail.com>
2017-07-07 08:31:12 +0700

    child constructor

--- stage_3/lesson_3.4_make_classes/b_advanced/inheritance.py
+++ stage_3/lesson_3.4_make_classes/b_advanced/inheritance.py
@@ -13,11 +13,20 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
-    def show_info(self):
-        print("Last Name - " + self.last_name)
-        print("Eye Color - " + self.eye_color)
+    # def show_info(self):
+    #     print("Last Name - " + self.last_name)
+    #     print("Eye Color - " + self.eye_color)
 
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child Constructor Called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+    # def show_info(self):
 
-    def show_info(self):
+# print billy_cyrus.last_name
+# billy_cyrus = Parent("Cyrus", "blue")
+
+miley_cyrus = Child("Cyrus", "Blue", 5)
+print miley_cyrus.last_name
+print miley_cyrus.number_of_toys
