2114bb2002e95c9760d6471cd560e2f445261e56
yasirabd <sir.yasirabd@gmail.com>
2017-07-07 09:28:51 +0700

    final project

---
+++ stage_3/project/media.py
@@ -0,0 +1,23 @@
+# Lesson 3.4: Make Classes
+# Mini-Project: Movies Website
+
+# In this file, you will define the class Movie. You could do this
+# directly in entertainment_center.py but many developers keep their
+# class definitions separate from the rest of their code. This also
+# gives you practice importing Python files.
+
+import webbrowser
+
+class Movie():
+    """This class provides a way to store movie related information"""
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+    def __init__(self, movie_title, movie_story_line, poster_image, trailer_youtube):
+        # initialize instance of class Movie
+        self.title = movie_title
+        self.storyline = movie_story_line
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
