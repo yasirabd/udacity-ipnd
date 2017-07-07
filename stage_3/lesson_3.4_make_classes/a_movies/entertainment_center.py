708b494a5939acda72219fe26dee67cd217448c7
yasirabd <sir.yasirabd@gmail.com>
2017-07-07 07:19:54 +0700

    game of thrones instance

--- stage_3/lesson_3.4_make_classes/a_movies/entertainment_center.py
+++ stage_3/lesson_3.4_make_classes/a_movies/entertainment_center.py
@@ -23,7 +23,15 @@ avatar = media.Movie("Avatar",
                     "https://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-print avatar.storyline
-avatar.show_trailer()
+# print avatar.storyline
+# avatar.show_trailer()
+
+game_of_thrones = media.Movie("Game of Thrones Season 7",
+                            "After a summer lasting almost ten years, the words of House Stark have finally become reality once again: winter has come.",
+                            "https://upload.wikimedia.org/wikipedia/en/0/04/Game_of_Thrones_Season_7.jpg",
+                            "https://www.youtube.com/watch?v=giYeaKsXnsI")
+
+game_of_thrones.show_trailer()
+
 # movies = [toy_story, avatar]
 # fresh_tomatoes.open_movies_page(movies)
