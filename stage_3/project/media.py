b22b5718a231a0056aae4a9bff6221540eca28d6
yasirabd <sir.yasirabd@gmail.com>
2017-07-12 13:24:35 +0700

    add comment in method

--- stage_3/project/media.py
+++ stage_3/project/media.py
@@ -8,16 +8,34 @@
 
 import webbrowser
 
-class Movie():
+class Movie(object):
     """This class provides a way to store movie related information"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     def __init__(self, movie_title, movie_story_line, poster_image, trailer_youtube):
-        # initialize instance of class Movie
+        """
+        Initialize instance of class Movie.
+        Arguments:
+        movie_title: title of the movie
+        movie_story_line: storyline of the movie
+        poster_image: image of the poster movie
+        trailer_youtube: video trailer of the movie
+        '''
+        Returns:
+        None
+        """
         self.title = movie_title
         self.storyline = movie_story_line
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        Open trailer youtube video in browser.
+        Arguments:
+        None
+        '''
+        Returns:
+        Open browser that contains url youtube video and play it.
+        """
         webbrowser.open(self.trailer_youtube_url)
